c4cd7d39cf89bb6b0a57e3996b64692a9ad1e210
Drew Daly <ad120136.drew@gmail.com>
2022-07-05 19:59:32 -0500

    Masthead MVP

---
+++ src/components/Masthead.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Masthead.scss";
+function Masthead() {
+  return (
+    <div className="masthead">
+    <div className="masthead-container">
+        <h1 className='title'>Peterson-Daly Farms LLC</h1>
+        <h3 className='subtitle'>Family Run Beef and Grain Farm</h3>
+    </div>
+</div>
+  )
+}
+
+export default Masthead
--- src/components/pages/HomePage/Home.js
+++ src/components/pages/HomePage/Home.js
@@ -3,10 +3,12 @@ import {Fade} from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 import HeroSection from '../../HeroSection';
 import {homeObjOne, homeObjTwo} from "./Data";
+import MastHead from '../../Masthead';
 
 const Home = () => {
   return (
     <>
+        <MastHead />
         <HeroSection {...homeObjOne} />
         <HeroSection {...homeObjTwo}/>
     </>
